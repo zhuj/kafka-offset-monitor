bb872478db9eb86fa45eb418bd47f1b55eb95422
Pierre Andrews <pierre.andrews@gmail.com>
2014-08-04 11:49:22 -0700

    a bit more args for command line access

--- src/main/scala/com/quantifind/kafka/offsetapp/OffsetGetterApp.scala
+++ src/main/scala/com/quantifind/kafka/offsetapp/OffsetGetterApp.scala
@@ -1,8 +1,10 @@
 package com.quantifind.kafka.offsetapp
 
+import com.quantifind.kafka.OffsetGetter.OffsetInfo
+
 import scala.concurrent.duration._
 
-import com.quantifind.sumac.{ArgMain, FieldArgs}
+import com.quantifind.sumac.{ ArgMain, FieldArgs }
 import com.quantifind.sumac.validation.Required
 import kafka.utils.ZKStringSerializer
 import org.I0Itec.zkclient.ZkClient
@@ -13,13 +15,16 @@ class OffsetGetterArgsWGT extends OffsetGetterArgs {
   var group: String = _
 
   var topics: Seq[String] = Seq()
+
+  var sumPart: Boolean = false
+
+  var onlyOffsets: Boolean = false
+
 }
 
 class OffsetGetterArgs extends FieldArgs {
   @Required
   var zk: String = _
-
-
 
   var zkSessionTimeout: Duration = 30 seconds
   var zkConnectionTimeout: Duration = 30 seconds
@@ -33,7 +38,6 @@ class OffsetGetterArgs extends FieldArgs {
  */
 object OffsetGetterApp extends ArgMain[OffsetGetterArgsWGT] {
 
-
   def main(args: OffsetGetterArgsWGT) {
     var zkClient: ZkClient = null
     var og: OffsetGetter = null
@@ -46,30 +50,53 @@ object OffsetGetterApp extends ArgMain[OffsetGetterArgsWGT] {
       val i = og.getInfo(args.group, args.topics)
 
       if (i.offsets.nonEmpty) {
-        println("%-15s\t%-40s\t%-3s\t%-15s\t%-15s\t%-15s\t%s".format("Group", "Topic", "Pid", "Offset", "logSize", "Lag", "Owner"))
-        i.offsets.foreach {
-          info =>
-            println("%-15s\t%-40s\t%-3s\t%-15s\t%-15s\t%-15s\t%s".format(info.group, info.topic, info.partition, info.offset, info.logSize, info.lag,
-              info.owner.getOrElse("none")))
+        if (!args.onlyOffsets) {
+          println()
         }
-        println()
-        println("Brokers")
-        i.brokers.foreach {
-          b =>
-            println(s"${b.id}\t${b.host}:${b.port}")
+        if (args.sumPart) {
+          if (!args.onlyOffsets) {
+            println("%-15s\t%-40s\t%-15s\t%-15s\t%-15s".format("Group", "Topic", "Offset", "logSize", "Lag"))
+          }
+          i.offsets.groupBy(info => (info.group, info.topic))
+            .flatMap {
+              case (_, infoGrp) =>
+                infoGrp.headOption map { head =>
+                  val (offset, log, lag) = infoGrp.foldLeft((0l, 0l, 0l)) {
+                    case ((offAcc, logAcc, lagAcc), info) =>
+                      (offAcc + info.offset, logAcc + info.logSize, lagAcc + info.lag)
+                  }
+                  "%-15s\t%-40s\t%-15s\t%-15s\t%-15s".format(head.group, head.topic, offset, log, lag)
+                }
+            }.foreach(println)
+        } else {
+          if (!args.onlyOffsets) {
+            println("%-15s\t%-40s\t%-3s\t%-15s\t%-15s\t%-15s\t%s".format("Group", "Topic", "Pid", "Offset", "logSize", "Lag", "Owner"))
+          }
+          i.offsets.foreach {
+            info =>
+              println("%-15s\t%-40s\t%-3s\t%-15s\t%-15s\t%-15s\t%s".format(info.group, info.topic, info.partition, info.offset, info.logSize, info.lag,
+                info.owner.getOrElse("none")))
+          }
+        }
+
+        if (!args.onlyOffsets) {
+          println()
+          println("Brokers")
+          i.brokers.foreach {
+            b =>
+              println(s"${b.id}\t${b.host}:${b.port}")
+          }
         }
       } else {
         System.err.println(s"no topics for group ${args.group}")
       }
 
-    }
-    finally {
+    } finally {
       if (og != null) og.close()
       if (zkClient != null)
         zkClient.close()
     }
   }
-
 
 }
 
