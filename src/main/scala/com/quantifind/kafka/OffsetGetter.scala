9dad3cdd189e3e1038d440f25c7f411ef056d30a
Pau Gay <pau.gay@gmail.com>
2015-04-10 09:08:00 +0200

    Added inactive consumers in '/topic/$TOPIC/consumers' and '/topicdetails/$TOPIC' endpoints.
    Changelog:
     - Added 'TopicAndConsumersDetailsWrapper' that wraps active and
       inactive 'TopicAndConsumersDetails' case class.
     - Added 'TopicDetailsWrapper' that wraps active and inactive
       'TopicDetails' case class.
     - Methods 'getTopicDetail()' and 'getTopicAndConsumersDetail()' now
       returns active and inactive consumers as well.
     - Created new few methods as a result of de-duplicating code.

--- src/main/scala/com/quantifind/kafka/OffsetGetter.scala
+++ src/main/scala/com/quantifind/kafka/OffsetGetter.scala
@@ -21,9 +21,11 @@ import scala.util.control.NonFatal
 
 case class Node(name: String, children: Seq[Node] = Seq())
 
-case class TopicDetails(consumers: Seq[ConsumerDetail])
+case class TopicDetails(active: Seq[ConsumerDetail], inactive: Seq[ConsumerDetail])
+case class TopicDetailsWrapper(consumers: TopicDetails)
 
-case class TopicAndConsumersDetails(consumers: Seq[KafkaInfo])
+case class TopicAndConsumersDetails(active: Seq[KafkaInfo], inactive: Seq[KafkaInfo])
+case class TopicAndConsumersDetailsWrapper(consumers: TopicAndConsumersDetails)
 
 case class ConsumerDetail(name: String)
 
@@ -106,18 +108,21 @@ class OffsetGetter(zkClient: ZkClient) extends Logging {
   }
 
   private def offsetInfo(group: String, topics: Seq[String] = Seq()): Seq[OffsetInfo] = {
-
     val topicList = if (topics.isEmpty) {
-      try {
-        ZkUtils.getChildren(
-          zkClient, s"${ZkUtils.ConsumersPath}/$group/offsets").toSeq
-      } catch {
-        case _: ZkNoNodeException => Seq()
-      }
+      getTopicList(group)
     } else {
       topics
     }
+
     topicList.sorted.flatMap(processTopic(group, _))
+  }
+
+  def getTopicList(group: String): List[String] = {
+    try {
+      ZkUtils.getChildren(zkClient, s"${ZkUtils.ConsumersPath}/$group/offsets").toList
+    } catch {
+      case _: ZkNoNodeException => List()
+    }
   }
 
   def getInfo(group: String, topics: Seq[String] = Seq()): KafkaInfo = {
@@ -140,37 +145,60 @@ class OffsetGetter(zkClient: ZkClient) extends Logging {
     }
   }
 
-
   /**
-   * returns details for a given topic such as the active consumers pulling off of it
-   * @param topic
-   * @return
+   * Returns details for a given topic such as the consumers pulling off of it
    */
-  def getTopicDetail(topic: String): TopicDetails = {
-    val topicMap = getActiveTopicMap
+  def getTopicDetail(topic: String): TopicDetailsWrapper = {
+    val topicMap = getTopicMap
+    val activeTopicMap = getActiveTopicMap
 
-    if (topicMap.contains(topic)) {
-      TopicDetails(topicMap(topic).map(consumer => {
-        ConsumerDetail(consumer.toString)
-      }).toSeq)
+    val activeConsumers = if (activeTopicMap.contains(topic)) {
+      mapConsumerDetails(activeTopicMap(topic))
     } else {
-      TopicDetails(Seq(ConsumerDetail("Unable to find Active Consumers")))
+      Seq(ConsumerDetail("Unable to find Active Consumers"))
     }
+
+    val inactiveConsumers = if (!activeTopicMap.contains(topic) && topicMap.contains(topic)) {
+      mapConsumerDetails(topicMap(topic))
+    } else if (activeTopicMap.contains(topic) && topicMap.contains(topic)) {
+      mapConsumerDetails(topicMap(topic).diff(activeTopicMap(topic)))
+    } else {
+      Seq(ConsumerDetail("Unable to find Inactive Consumers"))
+    }
+
+    TopicDetailsWrapper(TopicDetails(activeConsumers, inactiveConsumers))
   }
+
+  def mapConsumerDetails(consumers: Seq[String]): Seq[ConsumerDetail] =
+    consumers.map(consumer => ConsumerDetail(consumer.toString))
 
   /**
    * Returns details for a given topic such as the active consumers pulling off of it
    * and for each of the active consumers it will return the consumer data
    */
-  def getTopicAndConsumersDetail(topic: String): TopicAndConsumersDetails = {
-    val topicMap = getActiveTopicMap
+  def getTopicAndConsumersDetail(topic: String): TopicAndConsumersDetailsWrapper = {
+    val topicMap = getTopicMap
+    val activeTopicMap = getActiveTopicMap
 
-    if (topicMap.contains(topic)) {
-      TopicAndConsumersDetails(topicMap(topic).map(getInfo(_, Seq(topic))))
+    val activeConsumers = if (activeTopicMap.contains(topic)) {
+        mapConsumersToKafkaInfo(activeTopicMap(topic), topic)
     } else {
-      TopicAndConsumersDetails(Seq())
+        Seq()
     }
+
+    val inactiveConsumers = if (!activeTopicMap.contains(topic) && topicMap.contains(topic)) {
+      mapConsumersToKafkaInfo(topicMap(topic), topic)
+    } else if (activeTopicMap.contains(topic) && topicMap.contains(topic)) {
+      mapConsumersToKafkaInfo(topicMap(topic).diff(activeTopicMap(topic)), topic)
+    } else {
+      Seq()
+    }
+
+    TopicAndConsumersDetailsWrapper(TopicAndConsumersDetails(activeConsumers, inactiveConsumers))
   }
+
+  def mapConsumersToKafkaInfo(consumers: Seq[String], topic: String): Seq[KafkaInfo] =
+    consumers.map(getInfo(_, Seq(topic)))
 
   def getTopics: Seq[String] = {
     try {
@@ -185,9 +213,7 @@ class OffsetGetter(zkClient: ZkClient) extends Logging {
 
 
   /**
-   * returns a map of active topics-> list of consumers from zookeeper, ones that have IDS attached to them
-   *
-   * @return
+   * Returns a map of active topics -> list of consumers from zookeeper, ones that have IDS attached to them
    */
   def getActiveTopicMap: Map[String, Seq[String]] = {
     try {
@@ -203,6 +229,25 @@ class OffsetGetter(zkClient: ZkClient) extends Logging {
               error(s"could not get consumers for group $group", t)
               Seq()
           }
+      }.groupBy(_._1).mapValues {
+        _.unzip._2
+      }
+    } catch {
+      case NonFatal(t) =>
+        error(s"could not get topic maps because of ${t.getMessage}", t)
+        Map()
+    }
+  }
+
+  /**
+   * Returns a map of topics -> list of consumers, including non-active
+   */
+  def getTopicMap: Map[String, Seq[String]] = {
+    try {
+      ZkUtils.getChildren(zkClient, ZkUtils.ConsumersPath).flatMap {
+        group => {
+          getTopicList(group).map(topic => topic -> group)
+        }
       }.groupBy(_._1).mapValues {
         _.unzip._2
       }
